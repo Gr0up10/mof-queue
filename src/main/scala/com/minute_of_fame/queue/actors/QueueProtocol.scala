95c215d42f3732a6d9a77f447bbe6c115d17efd8
Yaroslav <maplegend@mail.ru>
2020-04-14 19:25:48 +0500

    Commit before json class refactoring

--- src/main/scala/com/minute_of_fame/queue/actors/QueueProtocol.scala
+++ src/main/scala/com/minute_of_fame/queue/actors/QueueProtocol.scala
@@ -6,8 +6,11 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.util.Timeout
 import com.minute_of_fame.queue.actors.QueueProtocol._
 import com.minute_of_fame.queue.models.DbModels.AuthUser
-import com.minute_of_fame.queue.models.JsonPackets.{AddToQueue, CommandPacket}
-import io.circe.parser.decode
+import com.minute_of_fame.queue.models.JsonPackets.{AddToQueue, Command, CommandPacket, SetStream, SetTime, UpdatePlace}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
 import scala.collection.mutable
 import scala.concurrent.duration._
@@ -15,19 +18,22 @@ import scala.concurrent.duration._
 object QueueProtocol {
   def props(db: ActorRef, qhandler: ActorRef) = Props(classOf[QueueProtocol], db, qhandler)
 
-  case class QueueUser()
-  case class AuthQueueUser(user: AuthUser) extends QueueUser
-  case class QueuePublisher(streamId: String, override val user: AuthUser) extends AuthQueueUser(user)
+  sealed abstract class StreamViewer
+  case class QueueViewer() extends StreamViewer
+  abstract class AuthViewer(user: AuthUser) extends StreamViewer
+  case class AuthQueueViewer(user: AuthUser) extends AuthViewer(user)
+  case class QueuePublisher(streamId: String, user: AuthUser) extends AuthViewer(user)
 }
 
 class QueueProtocol(db: ActorRef, qhandler: ActorRef) extends Actor with ActorLogging {
-  implicit val timeout = Timeout(2 seconds)
+  implicit val timeout = Timeout(2.seconds)
 
-  private val users = mutable.HashMap[Int, QueueUser]()
+  private val users = mutable.HashMap[Int, StreamViewer]()
 
   private var session: ActorRef = _
 
-  def make_packet()
+  def packCommand(id: Int, name: String, cmd: Command) =
+    packets.Packet(id, data=CommandPacket(name, cmd.asJson.noSpaces).asJson.noSpaces)
 
   override def receive: Receive = {
     case "connected" =>
@@ -41,11 +47,11 @@ class QueueProtocol(db: ActorRef, qhandler: ActorRef) extends Actor with ActorLo
             ask(db, DataBase.GetUser(pack.userId)) {
               case Success(DataBase.UserInfo(user)) =>
                 qhandler ! QueueHandler.Connected(pack.userId)
-                users += (pack.userId -> AuthQueueUser(user))
+                users += (pack.userId -> AuthQueueViewer(user))
 
               case Failure(exception) => log.warning("Cant get auth user {}:\n {}", pack.userId, exception)
             }
-          } else users += (pack.userId -> QueueUser())
+          } else users += (pack.userId -> QueueViewer())
 
         case "disconnected" =>
           if(pack.isAuth)
@@ -65,20 +71,36 @@ class QueueProtocol(db: ActorRef, qhandler: ActorRef) extends Actor with ActorLo
                     case Left(err) =>
                       log.error("Cant decode json packet {}:\n{}", json, err)
                   }
+
                 case "stop" =>
                   users(pack.userId) match {
                     case publisher: QueuePublisher =>
                       qhandler ! QueueHandler.StopStream(pack.userId)
-                      users(pack.userId) = AuthQueueUser(publisher.user)
+                      users(pack.userId) = AuthQueueViewer(publisher.user)
                     case _ =>
                       log.error("Cannot stop non existing stream for pack {}", pack)
                   }
               }
+
             case Left(err) =>
               log.error("Cant decode command json packet {}:\n{}", json, err)
           }
       }
-    case QueueHandler.SetTime =>
-      users.foreach(session ! packets.Packet)
+
+    case QueueHandler.SetTime(time) =>
+      users.keys.foreach(session ! packCommand(_, "set_time", SetTime(time)))
+
+    case QueueHandler.SetStream(stream, userId) =>
+      users.get(stream) match {
+        case Some(QueuePublisher(streamId, _)) =>
+          if(userId >= 0) session ! packCommand(userId, "set_stream", SetStream(streamId))
+          else users.keys
+            .filter(_ != userId).foreach(session ! packCommand(_, "set_stream", SetStream(streamId)))
+        case _ =>
+          log.error("Can't find publisher with id {}", stream)
+      }
+
+    case QueueHandler.UpdatePlaces(queue) =>
+      users.keys.foreach(session ! packCommand(_, "update_places", UpdatePlace(queue)))
   }
 }
--- src/main/scala/com/minute_of_fame/queue/models/JsonPackets.scala
+++ src/main/scala/com/minute_of_fame/queue/models/JsonPackets.scala
@@ -1,12 +1,12 @@
 package com.minute_of_fame.queue.models
 
 object JsonPackets {
-  abstract class Command
-  case class CommandPacket(command: String, data: Command)
+  case class CommandPacket(command: String, data: String)
 
+  abstract class Command
   case class AddToQueue(streamType: String, id: String) extends Command
 
   case class SetTime(time: Int) extends Command
-  case class UpdatePlace(queue: Array[String]) extends Command
+  case class UpdatePlace(queue: Array[Int]) extends Command
   case class SetStream(id: String) extends Command
 }
